c65e48f3fc479c252c3aa7d171aefe827b93aced
ahmad <ahmad.sadegh@gmail.com>
2026-08-04 23:16:54 +0330

    fix font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 
 export const metadata: Metadata = {
@@ -35,12 +24,6 @@ export default function RootLayout({
     <html
       lang="fa"
       dir="rtl"
-      className={`
-        ${geistSans.variable}
-        ${geistMono.variable}
-        h-full
-        antialiased
-      `}
     >
 
 
