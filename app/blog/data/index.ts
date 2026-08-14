fd68652acf40df777813349ddb796670a3095ec6
ahmad <ahmad.sadegh@gmail.com>
2026-08-14 12:56:05 +0330

    melasma,

--- app/blog/data/index.ts
+++ app/blog/data/index.ts
@@ -24,6 +24,7 @@ import { dermatitis } from "./dermatitis";
 import { fungus } from "./fungus";
 import { hives } from "./hives";
 import { impetigo } from "./impetigo";
+import { melasma } from "./melasma";
 import { latexAllergy } from "./latex-allergy";
 
 export const blogArticles: BlogArticle[] = [
@@ -36,4 +37,5 @@ export const blogArticles: BlogArticle[] = [
   hives,
   impetigo,
   latexAllergy,
+   melasma,
 ];
