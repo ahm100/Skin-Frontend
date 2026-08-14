0e469ab3bdce5d02852bd16d7bfc10b269c6a715
ahmad <ahmad.sadegh@gmail.com>
2026-08-14 16:29:44 +0330

    http instead of https in sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://www.surenmah.ir";
+  const baseUrl = "http://www.surenmah.ir";
 
   const blogSlugs = [
     "vitiligo",
