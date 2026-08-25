2f838a6f8a80b769f6ee79e3ec50d03e6ece99ec
sadegh.a <sadegh.a@toptec.ir>
2026-08-25 15:55:23 +0330

    before merge

--- lib/api.ts
+++ lib/api.ts
@@ -1,8 +1,8 @@
-// export const API_BASE =
-//   process.env.NEXT_PUBLIC_API_URL ?? "";
-
+// for VPS that we have container for backend
+export const API_BASE =
+  process.env.NEXT_PUBLIC_API_URL ?? "";
 
 //local without docker,...
-export const API_BASE =
- process.env.NEXT_PUBLIC_API_URL ?? "httpS://localhost:7004";
+// export const API_BASE =
+//  process.env.NEXT_PUBLIC_API_URL ?? "httpS://localhost:7004";
 
