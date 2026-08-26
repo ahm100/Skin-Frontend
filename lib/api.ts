0efdbfd0f96d97995a0e0c7b34ec4f1b53f40cf8
sadegh.a <sadegh.a@toptec.ir>
2026-08-26 14:32:16 +0330

    comment

--- lib/api.ts
+++ lib/api.ts
@@ -5,4 +5,6 @@ export const API_BASE =
 //local without docker,...
 // export const API_BASE =
 //  process.env.NEXT_PUBLIC_API_URL ?? "httpS://localhost:7004";
+// or create .env.local and inside it: 
+// NEXT_PUBLIC_API_URL=https://localhost:7004
 
